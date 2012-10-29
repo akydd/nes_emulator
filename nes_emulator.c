b030905cbeb2bd135376172d6ef407a88385e5fe
Alan Kydd <akydd@ualberta.net>
2012-10-28 23:04:15 -0600

    Refactored.  Added opcode logic for instruction lookup for each instruction set and address mode.  Consolidated adc functions into single function.

--- nes_emulator.c
+++ nes_emulator.c
@@ -17,10 +17,42 @@
  */
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+
+/* func declarations */
+/* setting flags */
+void add_set_carry_flag(uint8_t, uint8_t);
+void add_set_overflow_flag(uint8_t, uint8_t, uint8_t);
+void set_zero_flag(uint8_t);
+void set_negative_flag(uint8_t);
+/* processing instruction sets */
+void process_code(uint8_t);
+void process_00_code(uint8_t);
+void process_01_code(uint8_t);
+void process_10_code(uint8_t);
+/* instructions */
+void nul(uint8_t);
+void ora(uint8_t);
+void and(uint8_t);
+void eor(uint8_t);
+void adc(uint8_t);
+void sta(uint8_t);
+void lda(uint8_t);
+void cmp(uint8_t);
+void sbc(uint8_t);
+/* addressing modes */
+uint8_t get_zero_page_indexed_indirect(void);
+uint8_t get_zero(void);
+uint8_t get_immediate(void);
+uint8_t get_absolute(void);
+uint8_t get_zero_page_indirect_index(void);
+uint8_t get_zero_x(void);
+uint8_t get_absolute_x(void);
+uint8_t get_absolute_y(void);
 
 #define MEM_SIZE 2 * 1024
 
-/* memory */
+/* 2 kilobytes of memory */
 uint8_t memory[MEM_SIZE];
 
 /* registers, all unsigned */
@@ -44,99 +76,221 @@ int main(void)
 	return 0;
 }
 
-void adc_imm()
+void process_code(uint8_t code)
 {
-	PC++;
-	uint16_t addr = PC;
-	uint8_t val = memory[addr];
-	val += (P & C_FLAG);
-	uint8_t sum = A + val;
+	/* opcodes are formatted as aaabbbcc, where aaa and cc determine
+	 * the instruction, and bbb determines the addressing mode. */
 
-	add_set_c_flag(A, val);
-	add_set_v_flag(A, val, sum);
+	static void (* const pf[]) (uint8_t) = {
+		&process_00_code, &process_01_code, &process_10_code
+	};
 
-	A = sum;
-
-	PC++;
+	/* determine the instruction set.  Cases for cc = 00, 01, or 10 */
+	uint8_t cc = code & 0x03;
+	if(cc < sizeof(pf) / sizeof(*pf)) {
+		pf[cc](code);
+	}
 }
 
-void adc_zero()
+void process_00_code(uint8_t code)
 {
-	PC++;
+	static void (* const pf[]) (uint8_t) = {
+		&nul, &bit, &jmp, &jmp_abs, &sty, &ldy, &cpy, &cpx
+	};
+
+	/* determine the instruction and addressing mode. */
+	uint8_t aaa = (code>>5) & 0x07;
+	uint8_t bbb = (code>>2) & 0x07;
+
+	if(aaa < sizeof(pf) / sizeof(*pf)) {
+		pf[aaa](bbb);
+	}
+}
+
+void process_01_code(uint8_t code)
+{
+	static void (* const pf[]) (uint8_t) = {
+		&ora, &and, &eor, &adc, &sta, &lda, &cmp, &sbc
+	};
+
+	/* determine the instruction and addressing mode. */
+	uint8_t aaa = (code>>5) & 0x07;
+	uint8_t bbb = (code>>2) & 0x07;
+
+	if(aaa < sizeof(pf) / sizeof(*pf)) {
+		pf[aaa](bbb);
+	}
+}
+
+void process_10_code(uint8_t code)
+{
+	static void (* const pf[]) (uint8_t) = {
+		&asl, &rol, &lsr, &ror, &stx, &ldx, &dec, &inc
+	};
+
+	/* determine the instruction and addressing mode. */
+	uint8_t aaa = (code>>5) & 0x07;
+	uint8_t bbb = (code>>2) & 0x07;
+
+	if(aaa < sizeof(pf) / sizeof(*pf)) {
+		pf[aaa](bbb);
+	}
+}
+/* 
+ * common memory access functions for addressing modes.
+ * This mode determines which value gets used in the computation, so these
+ * functions can be reused for all instructions.
+ */
+
+/*
+ * Accumulator mode return the value in the accumulator
+ */
+uint8_t get_accumulator()
+{
+	return A;
+}
+
+/*
+ * Immediate mode returns the operand after the instr
+ */
+uint8_t get_immediate()
+{
+	return memory[PC];
+}
+
+/*
+ * Absolute mode returns the value at the 16-bit address at the PC
+ */
+uint8_t get_absolute()
+{
 	uint16_t addr = memory[PC];
-	uint8_t val = memory[addr];
-	val += (P & C_FLAG);
-	uint8_t sum = A + val;
-
-	add_set_c_flag(A, val);
-	add_set_v_flag(A, val, sum);
-
-	A = sum;
-
+	addr = addr << 8;
 	PC++;
+	addr |= memory[PC];
+
+	return memory[addr];
 }
 
-void adc_zero_x()
+/*
+ * Zero Page mode returns the byte at the address given by the operand
+ */
+uint8_t get_zero()
 {
-	PC++;
+	uint8_t addr = memory[PC];
+	return memory[addr];
+}
+
+/*
+ * Zero Page index with X mode returns the value at the address given by the sum
+ * of the operand and X.
+ */
+uint8_t get_zero_x()
+{
+	uint8_t addr = memory[PC] + X;
+	return memory[addr];
+}
+
+/*
+ * Zero Page index with Y mode returns the value at the address given by the sum
+ * of the operand and Y.
+ */
+uint8_t get_zero_y()
+{
+	uint8_t addr = memory[PC] + Y;
+	return memory[addr];
+}
+
+/*
+ * Absolute index with X mode returns the value at the address given by the
+ * sum of X and the operand.
+ */
+uint8_t get_absolute_x()
+{
 	uint16_t addr = memory[PC] + X;
-	uint8_t val = memory[addr];
-	val += (P & C_FLAG);
-	uint8_t sum = A + val;
-
-	add_set_c_flag(A, val);
-	add_set_v_flag(A, val, sum);
-
-	A = sum;
-
-	PC++;
+	return memory[addr];
 }
 
-void adc_abs()
+/*
+ * Absolute index with Y mode returns the value at the address given by the
+ * sum of Y and the operand.
+ */
+uint8_t get_absolute_y()
+{
+	uint16_t addr = memory[PC] + Y;
+	return memory[addr];
+}
+
+/*
+ * Zero Page indexed indirect mode returns the value at the 2-byte address given
+ * by the values of the memory at addresses (operand + X) and (operand + X + 1).
+ */
+uint8_t get_zero_page_indexed_indirect()
+{
+	uint16_t addr = memory[PC + X];
+	addr = addr << 8;
+	addr |= memory[PC + X + 1];
+	return memory[addr];
+}
+
+/*
+ * Zero Page indirect indexed with Y mode returns the value at the 2-byte
+ * address given by the ((value at the address of the operand) + Y) and ((value
+ * at the address of the operand) + Y) + 1.
+ */
+uint8_t get_zero_page_indirect_index_y()
+{
+	uint16_t addr = memory[PC] + Y;
+	addr = addr << 8;
+	addr |= memory[PC] + Y + 1;
+	return memory[addr];
+}
+
+uint8_t get_addr_mode_01_value(mode)
+{
+	static uint8_t (* const pf[]) (void) = {
+		&get_zero_page_indexed_indirect,
+		&get_zero,
+		&get_immediate,
+		&get_absolute,
+		&get_zero_page_indirect_index_y,
+		&get_zero_x,
+		&get_absolute_x,
+		&get_absolute_y
+	};
+
+	if(mode < sizeof(pf) / sizeof(*pf)) {
+		return pf[mode]();
+	}
+	return -1;
+}
+
+void nul(uint8_t mode)
+{
+	(void)printf("No such code!\n");
+}
+
+void adc(uint8_t mode)
 {
 	PC++;
-	uint16_t addr = memory[PC];
-	addr = addr << 8;
-	PC++;
-	addr |= memory[PC];
 
-	uint8_t val = memory[addr];
+	uint8_t val = get_addr_mode_01_value(mode);
 	val += (P & C_FLAG);
 	uint8_t sum = A + val;
 
-	add_set_c_flag(A, val);
-	add_set_v_flag(A, val, sum);
+	add_set_carry_flag(A, val);
+	add_set_overflow_flag(A, val, sum);
+	set_negative_flag(sum);
+	set_zero_flag(sum);
 
 	A = sum;
 
 	PC++;
 }
-
-void adc_abs_x()
-{
-	PC++;
-	uint16_t addr = memory[PC];
-	addr = addr << 8;
-	PC++;
-	addr |= memory[PC];
-
-	uint8_t val = memory[addr + X];
-	val += (P & C_FLAG);
-	uint8_t sum = A + val;
-
-	add_set_c_flag(A, val);
-	add_set_v_flag(A, val, sum);
-
-	A = sum;
-
-	PC++;
-}
-
 
 /*
  * Determine if the carry flag should be set when adding a and b.
  */
-void add_set_c_flag(uint8_t a, uint8_t b)
+void add_set_carry_flag(uint8_t a, uint8_t b)
 {
 	/* if carry flag is zero, check if a + b > 0xff.
 	 * Otherwise, check if a + b >= 0xff. */
@@ -154,7 +308,7 @@ void add_set_c_flag(uint8_t a, uint8_t b)
 /*
  * Determine if the overflow flag should be set after adding a and b.
  */
-void add_set_v_flag(uint8_t a, uint8_t b, uint8_t sum)
+void add_set_overflow_flag(uint8_t a, uint8_t b, uint8_t sum)
 {
 	int overflow = 0;
 	if((((a|b) ^ sum) & 0x80) == 0x80) {
@@ -163,5 +317,19 @@ void add_set_v_flag(uint8_t a, uint8_t b, uint8_t sum)
 
 	if((overflow == 1) ^ ((P & C_FLAG) == C_FLAG)) {
 		P |= V_FLAG;
+	}
+}
+
+void set_zero_flag(uint8_t a)
+{
+	if (a == 0) {
+		P |= Z_FLAG;
+	}
+}
+
+void set_negative_flag(uint8_t a)
+{
+	if ((a & N_TEST) == N_TEST) {
+		P |= N_FLAG;
 	}
 }
