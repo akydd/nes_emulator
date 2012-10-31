5a5b263e03a688160b13619c36a52ab0dc5a6b7d
Alan Kydd <akydd@ualberta.net>
2012-10-30 22:52:17 -0600

    Refactored memory access functions.  Implemented more instructions.  Will not compile yet.

--- nes_emulator.c
+++ nes_emulator.c
@@ -41,14 +41,15 @@ void lda(uint8_t);
 void cmp(uint8_t);
 void sbc(uint8_t);
 /* addressing modes */
-uint8_t get_zero_page_indexed_indirect(void);
-uint8_t get_zero(void);
-uint8_t get_immediate(void);
-uint8_t get_absolute(void);
-uint8_t get_zero_page_indirect_index(void);
-uint8_t get_zero_x(void);
-uint8_t get_absolute_x(void);
-uint8_t get_absolute_y(void);
+uint16_t get_accumulator(void);
+uint16_t get_zero_page_indirect_index_y(void);
+uint16_t get_zero(void);
+uint16_t get_immediate(void);
+uint16_t get_absolute(void);
+uint16_t get_zero_page_indexed_indirect(void);
+uint16_t get_zero_x(void);
+uint16_t get_absolute_x(void);
+uint16_t get_absolute_y(void);
 
 #define MEM_SIZE 2 * 1024
 
@@ -75,6 +76,30 @@ int main(void)
 {
 	return 0;
 }
+
+/* function ptr array for memory access modes when cc = 01 */
+static uint16_t (* const pf_01[]) (void) = {
+	&get_zero_page_indexed_indirect,
+	&get_zero,
+	&get_immediate,
+	&get_absolute,
+	&get_zero_page_indirect_index_y,
+	&get_zero_x,
+	&get_absolute_y,
+	&get_absolute_x
+};
+
+/* function ptr array for memory access modes when cc = 00 or cc = 10 */
+static uint16_t (* const pf[]) (void) = {
+	&get_immediate,
+	&get_zero,
+	&get_accumulator,
+	&get_absolute,
+	NULL,
+	&get_zero_x,
+	NULL,
+	&get_absolute_x
+};
 
 void process_code(uint8_t code)
 {
@@ -145,123 +170,123 @@ void process_10_code(uint8_t code)
 /*
  * Accumulator mode return the value in the accumulator
  */
-uint8_t get_accumulator()
+uint16_t get_accumulator()
 {
-	return A;
+	return (uint16_t)A;
 }
 
 /*
  * Immediate mode returns the operand after the instr
  */
-uint8_t get_immediate()
+uint16_t get_immediate()
 {
-	return memory[PC];
+	return PC;
 }
 
 /*
- * Absolute mode returns the value at the 16-bit address at the PC
+ * Absolute mode returns the 16-bit address at the PC
  */
-uint8_t get_absolute()
+uint16_t get_absolute()
 {
 	uint16_t addr = memory[PC];
 	addr = addr << 8;
 	PC++;
 	addr |= memory[PC];
 
-	return memory[addr];
+	return addr;
 }
 
 /*
- * Zero Page mode returns the byte at the address given by the operand
+ * Zero Page mode returns the address given by the operand
  */
-uint8_t get_zero()
+uint16_t get_zero()
 {
-	uint8_t addr = memory[PC];
-	return memory[addr];
+	uint16_t addr = (uint16_t)memory[PC];
+	return addr;
 }
 
 /*
- * Zero Page index with X mode returns the value at the address given by the sum
+ * Zero Page index with X mode returns the address given by the sum
  * of the operand and X.
  */
-uint8_t get_zero_x()
+uint16_t get_zero_x()
 {
-	uint8_t addr = memory[PC] + X;
-	return memory[addr];
+	uint16_t addr = (uint16_t)memory[PC] + X;
+	return addr;
 }
 
 /*
- * Zero Page index with Y mode returns the value at the address given by the sum
+ * Zero Page index with Y mode returns the address given by the sum
  * of the operand and Y.
  */
-uint8_t get_zero_y()
+uint16_t get_zero_y()
 {
-	uint8_t addr = memory[PC] + Y;
-	return memory[addr];
+	uint16_t addr = (uint16_t)memory[PC] + Y;
+	return addr;
 }
 
 /*
- * Absolute index with X mode returns the value at the address given by the
+ * Absolute index with X mode returns the address given by the
  * sum of X and the operand.
  */
-uint8_t get_absolute_x()
+uint16_t get_absolute_x()
 {
-	uint16_t addr = memory[PC] + X;
-	return memory[addr];
+	uint16_t addr = (uint16_t)memory[PC] + X;
+	return addr;
 }
 
 /*
- * Absolute index with Y mode returns the value at the address given by the
+ * Absolute index with Y mode returns the address given by the
  * sum of Y and the operand.
  */
-uint8_t get_absolute_y()
+uint16_t get_absolute_y()
 {
-	uint16_t addr = memory[PC] + Y;
-	return memory[addr];
+	uint16_t addr = (uint16_t)memory[PC] + Y;
+	return addr;
 }
 
 /*
- * Zero Page indexed indirect mode returns the value at the 2-byte address given
+ * Zero Page indexed indirect mode returns the 2-byte address given
  * by the values of the memory at addresses (operand + X) and (operand + X + 1).
  */
-uint8_t get_zero_page_indexed_indirect()
+uint16_t get_zero_page_indexed_indirect()
 {
-	uint16_t addr = memory[PC + X];
+	uint16_t addr = (uint16_t)memory[PC + X];
 	addr = addr << 8;
 	addr |= memory[PC + X + 1];
-	return memory[addr];
+	return addr;
 }
 
 /*
- * Zero Page indirect indexed with Y mode returns the value at the 2-byte
+ * Zero Page indirect indexed with Y mode returns the 2-byte
  * address given by the ((value at the address of the operand) + Y) and ((value
  * at the address of the operand) + Y) + 1.
  */
-uint8_t get_zero_page_indirect_index_y()
+uint16_t get_zero_page_indirect_index_y()
 {
-	uint16_t addr = memory[PC] + Y;
+	uint16_t addr = (uint16_t)memory[PC] + Y;
 	addr = addr << 8;
 	addr |= memory[PC] + Y + 1;
-	return memory[addr];
+	return addr;
 }
 
-uint8_t get_addr_mode_01_value(mode)
+uint8_t read_addr_mode_01(uint8_t mode)
 {
-	static uint8_t (* const pf[]) (void) = {
-		&get_zero_page_indexed_indirect,
-		&get_zero,
-		&get_immediate,
-		&get_absolute,
-		&get_zero_page_indirect_index_y,
-		&get_zero_x,
-		&get_absolute_x,
-		&get_absolute_y
-	};
-
-	if(mode < sizeof(pf) / sizeof(*pf)) {
-		return pf[mode]();
+	if(mode < sizeof(pf_01) / sizeof(*pf_01)) {
+		return memory[pf_01[mode]()];
 	}
-	return -1;
+	(void)printf("No such addressing mode!\n");
+	exit(EXIT_FAILURE);
+}
+
+void write_addr_mode_01(uint8_t mode, uint8_t byte)
+{
+	if(mode < sizeof(pf_01) / sizeof(*pf_01)) {
+		memory[pf_01[mode]()] = byte;
+	} else {
+		(void)printf("No such addressing mode!\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 void nul(uint8_t mode)
@@ -269,11 +294,59 @@ void nul(uint8_t mode)
 	(void)printf("No such code!\n");
 }
 
+/*
+ * bitwise or of operand and the accumulator, stored in accumulator
+ */
+void ora(uint8_t mode)
+{
+	PC++;
+
+	uint8_t val = read_addr_mode_01(mode);
+	A |= val;
+
+	set_negative_flag(A);
+	set_zero_flag(A);
+
+	PC++;
+}
+
+/*
+ * bitwise and of operand and the accumulator, stored in accumulator
+ */
+void and(uint8_t mode)
+{
+	PC++;
+
+	uint8_t val = read_addr_mode_01(mode);
+	A &= val;
+
+	set_negative_flag(A);
+	set_zero_flag(A);
+
+	PC++;
+}
+
+/*
+ * Exclusive or of operand and the accumulator, stored in accumulator
+ */
+void eor(uint8_t mode)
+{
+	PC++;
+
+	uint8_t val = read_addr_mode_01(mode);
+	A ^= val;
+
+	set_negative_flag(A);
+	set_zero_flag(A);
+
+	PC++;
+}
+
 void adc(uint8_t mode)
 {
 	PC++;
 
-	uint8_t val = get_addr_mode_01_value(mode);
+	uint8_t val = read_addr_mode_01(mode);
 	val += (P & C_FLAG);
 	uint8_t sum = A + val;
 
@@ -283,6 +356,55 @@ void adc(uint8_t mode)
 	set_zero_flag(sum);
 
 	A = sum;
+
+	PC++;
+}
+
+/*
+ * Store the accumulator value to the memory location given by the mode and
+ * opcode.
+ */
+void sta(uint8_t mode)
+{
+	PC++;
+
+	write_addr_mode_01(mode, A);
+}
+
+/*
+ * Load a byte of memory into the accumulator
+ */
+void lda(uint8_t mode)
+{
+	PC++;
+
+	uint8_t val = read_addr_mode_01(mode);
+	A = val;
+
+	set_negative_flag(A);
+	set_zero_flag(A);
+
+	PC++;
+}
+
+/* 
+ * compare Accumulator to value at location of operand:
+ * A < val => set negative flag
+ * A > val => set carry flag
+ * A == val => set zero flag
+ */
+void cmp(uint8_t mode)
+{
+	PC++;
+
+	uint8_t val = read_addr_mode_01(mode);
+	if(A < val) {
+		P |= N_FLAG;
+	} else if (A > val) {
+		P |= C_FLAG;
+	} else {
+		P |= Z_FLAG;
+	}
 
 	PC++;
 }
