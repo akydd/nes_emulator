bd91764890ec255dfc4dc87b9c361f29739b3236
Alan Kydd <akydd@ualberta.net>
2013-04-24 23:34:47 -0600

    Psuedo code for combined PPU-CPU execution

--- nes_emulator.c
+++ nes_emulator.c
@@ -20,10 +20,38 @@
 #include <stdio.h>
 #include "cpu.h"
 #include "instructions.h"
+#include "memory.h"
 
 /* func declarations */
 
 int main(void)
 {
+	/*
+	 * Load file into memory
+	 */
+
+	/*
+	 * Initialize CPU with cpu->PC and cpu->S pointing to memory.
+	 */
+	
+	/*
+	 * Initialize PPU with registers set to 0x2000 to 0x2007 in main mem.
+	 */
+
+	/* Execution:
+	 * for(;;)
+	 * {
+	 *	int op_code = &(cpu->PC);
+	 *	int cpu_cycles = cycles[op_code];
+	 *
+	 *	op[op_code]();
+	 *
+	 *	int ppu_cycles = 3 * cpu_cycles;
+	 *	while(ppu_cycles > 0)
+	 *	{
+	 *		ppu_execute();
+	 *	}
+	 * }
+	 */
 	return 0;
 }
