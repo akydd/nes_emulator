b51644cc9ea8dab15fc9fee68e2bcf7a59f0a9c6
Alan Kydd <akydd@ualberta.net>
2014-03-26 22:28:31 -0600

    Added more BG rendering info.

--- ppu_memory.h
+++ ppu_memory.h
@@ -79,28 +79,60 @@ struct ppu_memory;
  * |__________________| 0x0000
  *
  *
+ * Backgroud Palettes
+ * ==================
+ * There are 4 background palettes of 4 colors, 1 byte per color, for a total of
+ * 16 bytes.
+ *
+ *
+ * Colors
+ * ======
+ * Colors take values 0x00 to 0x3F.
+ *
+ * The NES PPU does not use RGB, it uses NTSC to generate the colors.
+ * http://wiki.nesdev.com/w/index.php/NTSC_video
+ *
+ * For list of the approx RGB values for these colors see
+ * http://wiki.nesdev.com/w/index.php/PPU_palettes#RGB_palette_values
+ *
+ *
  * Name Tables
  * ===========
  * Each name table contains 960 bytes, which split up the screen into 30
  * rows, top to bottom, each with 32 columns, left to right.  Usually
  * the top and bottom most rows are not visible.  Each cell is 8x8 pixels.
  *
+ *
  * Attribute Tables
  * ================
  * Each attribute table contains 64 bytes, or 512 bits, which are used to color
  * in the above 960 tiles, via compression.
  *
- * There are only 4 background palette, which needs only 2 bits to represent, so
- * each attribute table contains 256 background palette assignments.  Rounding
- * up, this means that 4 tiles must be handled by each palette assignment, or
+ * The data stored is the upper two bits of the background tiles colors, indexed
+ * by 4 color palettes.  Each color palette contains 4 colors.  So
+ * each attribute table contains 256 background color assignments.  Rounding
+ * up, this means that 4 tiles must be handled by each color assignment, or
  * that 16 tiles must be handled by each byte in the attribute table.  This is
  * accomplished by grouping the 960 cells in the name tables into larger cells
  * of 4x4 name table cells, subdivided into 4 quadrants of 2x2 name table cells.
  * Each of these larger cells is 32x32 pixels.
  *
- * The assignment of palettes bytes to the 32x32 pixel cells is AABBCCDD to
+ * The assignment of each bytes to a 32x32 pixel cell is AABBCCDD to
  * DD CC
  * BB AA
+ *
+ *
+ * Pattern Tables
+ * ==============
+ * Each pattern table is 4096 bytes.  An 8x8 pixel tile uses 16 bytes, 2 bytes
+ * per 8X1 pixel row, for a total of 256 tiles.  For each 8x1 pixel row, the
+ * first byte holds the low color bit, and the second byte holds the high color bit.
+ * 
+ *
+ * Putting these together
+ * ======================
+ * Each 32x32 tile can contain at most 16 colors.
+ *
  *
  * Each scanline, the PPU looks into the name tables 34 times and draws 8x1 pixels
  * for each table for the first 33 of those fetches.
